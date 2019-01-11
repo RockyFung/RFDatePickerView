11514925ddabd44ae484c9f75657ffc63bc2c3c5
冯剑 <fengjian0213@126.com>
2019-01-11 09:45:10 +0800

    Initial Commit

---
+++ RFDatePickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RFDatePickerView
+//
+//  Created by 冯剑 on 2019/1/11.
+//  Copyright © 2019 rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RFDatePickerView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RFDatePickerView
+//
+//  Created by 冯剑 on 2019/1/11.
+//  Copyright © 2019 rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
